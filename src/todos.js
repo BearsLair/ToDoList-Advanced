d9ddca95000f22290757ed448ee59ff231204788
Patrick Gross <bears_lair@protonmail.com>
2024-12-16 18:35:13 -0800

    functions/comments added to module

--- src/todos.js
+++ src/todos.js
@@ -1,3 +1,5 @@
+// Instantiates a new project that will include list of todos
+// Method to change the project title is included
 class project {
   constructor(projectTitle) {
     this.projectTitle = projectTitle;
@@ -8,32 +10,36 @@ class project {
   }
 }
 
-class task {
-  constructor(todoTitle, description, dueDate, notes) {
-    this.todoTitle = todoTitle;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.notes = notes;
-    this.checked = false;
-  }
+// Creates new to-do item with title, description, due date, and notes
+// Methods are included to change elements of the to-do items
+const createNewTask = (todoTitle, description, dueDate, notes) => {
+  class task {
+    constructor(todoTitle, description, dueDate, notes) {
+      this.todoTitle = todoTitle;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.notes = notes;
+      this.checked = false;
+    }
 
-  changeTitle(newTitle) {
-    this.todoTitle = newTitle;
-  }
+    changeTitle(newTitle) {
+      this.todoTitle = newTitle;
+    }
 
-  changeDesc(newDesc) {
-    this.description = newDesc;
-  }
+    changeDesc(newDesc) {
+      this.description = newDesc;
+    }
 
-  changeDueDate(newDueDate) {
-    this.dueDate = newDueDate;
-  }
+    changeDueDate(newDueDate) {
+      this.dueDate = newDueDate;
+    }
 
-  changeNotes(newNotes) {
-    this.notes = newNotes;
-  }
+    changeNotes(newNotes) {
+      this.notes = newNotes;
+    }
 
-  changeCheck(change) {
-    this.checked = change;
+    changeCheck(change) {
+      this.checked = change;
+    }
   }
-}
+};
