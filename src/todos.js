17e25256f50f4fed949371000548888b4856613b
Patrick Gross <bears_lair@protonmail.com>
2024-12-16 18:20:26 -0800

    basic constructors and methods set up

--- src/todos.js
+++ src/todos.js
@@ -9,15 +9,27 @@ class project {
 }
 
 class task {
-  constructor(todoTitle, description, dueDate, notes, checked) {
+  constructor(todoTitle, description, dueDate, notes) {
     this.todoTitle = todoTitle;
     this.description = description;
     this.dueDate = dueDate;
     this.notes = notes;
-    this.checked = checked;
+    this.checked = false;
   }
 
-  change(newTitle) {
+  changeTitle(newTitle) {
     this.todoTitle = newTitle;
+  }
+
+  changeDesc(newDesc) {
+    this.description = newDesc;
+  }
+
+  changeDueDate(newDueDate) {
+    this.dueDate = newDueDate;
+  }
+
+  changeNotes(newNotes) {
+    this.notes = newNotes;
   }
 }
