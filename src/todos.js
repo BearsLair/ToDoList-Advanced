ed04c8c58edc311a9c673ce62215206b7a950604
Patrick Gross <bears_lair@protonmail.com>
2024-12-17 23:19:46 -0800

    removed ids for the time being

--- src/todos.js
+++ src/todos.js
@@ -1,9 +1,9 @@
+var uniqid = require("uniqid");
 // Instantiates a new project that will include list of todos
 // Method to change the project title is included
 // Method to add/delete todos to a project is also present
-class project {
-  constructor(id, projectTitle) {
-    this.id = id;
+class Project {
+  constructor(projectTitle) {
     this.projectTitle = projectTitle;
     this.currentToDos = [];
   }
@@ -16,41 +16,41 @@ class project {
     this.currentToDos.push(newToDo);
   }
 
-  deleteTodo(todo) {
-    // TODO: code neede here
+  deleteTodo(index) {
+    this.currentToDos.splice(index, index + 1);
   }
 }
 
 // Creates new to-do item with title, description, due date, and notes
 // Methods are included to change elements of the to-do items
-const createNewTask = (todoTitle, description, dueDate, notes) => {
-  class task {
-    constructor(todoTitle, description, dueDate, notes) {
-      this.todoTitle = todoTitle;
-      this.description = description;
-      this.dueDate = dueDate;
-      this.notes = notes;
-      this.checked = false;
-    }
-
-    changeTitle(newTitle) {
-      this.todoTitle = newTitle;
-    }
-
-    changeDesc(newDesc) {
-      this.description = newDesc;
-    }
-
-    changeDueDate(newDueDate) {
-      this.dueDate = newDueDate;
-    }
-
-    changeNotes(newNotes) {
-      this.notes = newNotes;
-    }
-
-    changeCheck(change) {
-      this.checked = change;
-    }
+class Task {
+  constructor(todoTitle, description, dueDate, notes) {
+    this.todoTitle = todoTitle;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.notes = notes;
+    this.checked = false;
   }
-};
+
+  changeTitle(newTitle) {
+    this.todoTitle = newTitle;
+  }
+
+  changeDesc(newDesc) {
+    this.description = newDesc;
+  }
+
+  changeDueDate(newDueDate) {
+    this.dueDate = newDueDate;
+  }
+
+  changeNotes(newNotes) {
+    this.notes = newNotes;
+  }
+
+  changeCheck(change) {
+    this.checked = change;
+  }
+}
+
+export { Project, Task };
