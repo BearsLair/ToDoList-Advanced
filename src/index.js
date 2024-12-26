bceb7df91a5533f7063b8142a890a026886d14b8
Patrick Gross <bears_lair@protonmail.com>
2024-12-26 14:54:48 -0800

    removed project tracking array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import { Project, Task } from "./todos";
-import submitProjectModal from "./project_submit";
+import { newProjectModal, projectSubmit } from "./projects";
 import "./styles.css";
-
-let allProjects = [];
-let currentViewedProject = null;
 
 const newProject = document.querySelector("#newProject");
 
 newProject.addEventListener("click", () => {
-  submitProjectModal();
+  newProjectModal();
 });
