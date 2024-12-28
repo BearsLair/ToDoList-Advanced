ecb96e22f2c7fb5b8bfefd2d582d9699467ae8c9
Patrick Gross <bears_lair@protonmail.com>
2024-12-27 21:29:03 -0800

    made variables available from project module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { Project, Task } from "./todos";
-import { newProjectModal, projectSubmit } from "./projects";
+import { newProjectModal, allProjects, currentViewedProject } from "./projects";
 import "./styles.css";
 
 const newProject = document.querySelector("#newProject");
 
 newProject.addEventListener("click", () => {
+  console.log("Current Projects before new one: ", allProjects);
+  console.log("Currently viewed project: ", currentViewedProject);
   newProjectModal();
 });
