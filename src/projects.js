c0a047c7f84817d59b65b2780a5963d846cd9c8a
Patrick Gross <bears_lair@protonmail.com>
2024-12-27 21:29:33 -0800

    creating new project is now functional

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,32 @@
+// Instantiates a new project that will include list of todos
+// Method to change the project title is included
+// Method to add/delete todos to a project is also present
+class Project {
+  constructor(projectTitle) {
+    this.projectTitle = projectTitle;
+    this.currentToDos = [];
+  }
+
+  change(newTitle) {
+    this.projectTitle = newTitle;
+  }
+
+  addTodo(newToDo) {
+    this.currentToDos.push(newToDo);
+  }
+
+  deleteTodo(index) {
+    this.currentToDos.splice(index, index + 1);
+  }
+}
+
 let allProjects = [];
 let currentViewedProject = null;
 
+//Constructs new project dialog box
 const newProjectModal = () => {
   const modalDiv = document.querySelector(".modal-div");
+  modalDiv.innerHTML = "";
   modalDiv.style.display = "block";
 
   const desc = document.createElement("p");
@@ -20,16 +44,12 @@ const newProjectModal = () => {
   projectSubmitBtn.textContent = "Submit";
 
   projectSubmitBtn.addEventListener("click", () => {
-    allProjects.push(userInput.textContent);
+    let project = new Project(userInput.value);
+    allProjects.push(project);
+    currentViewedProject = project.projectTitle;
     console.log(allProjects);
     modalDiv.style.display = "none";
   });
 };
 
-const projectSubmit = () => {
-  allProjects.push(userInput.textContent);
-  console.log(allProjects);
-  modalDiv.style.display = "none";
-};
-
-export { newProjectModal, projectSubmit };
+export { newProjectModal, allProjects, currentViewedProject };
