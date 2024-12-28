57fd98836198df45f42fadb7a29eac8ebad1ee7a
Patrick Gross <bears_lair@protonmail.com>
2024-12-27 21:28:14 -0800

    created separate todos module with class

--- src/todos.js
+++ src/todos.js
@@ -1,25 +1,3 @@
-// Instantiates a new project that will include list of todos
-// Method to change the project title is included
-// Method to add/delete todos to a project is also present
-class Project {
-  constructor(projectTitle) {
-    this.projectTitle = projectTitle;
-    this.currentToDos = [];
-  }
-
-  change(newTitle) {
-    this.projectTitle = newTitle;
-  }
-
-  addTodo(newToDo) {
-    this.currentToDos.push(newToDo);
-  }
-
-  deleteTodo(index) {
-    this.currentToDos.splice(index, index + 1);
-  }
-}
-
 // Creates new to-do item with title, description, due date, and notes
 // Methods are included to change elements of the to-do items
 class Task {
