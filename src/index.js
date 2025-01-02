4f7f4baa7e55ccad5165b883d962249fd31518d8
Patrick Gross <bears_lair@protonmail.com>
2025-01-01 16:14:50 -0800

    added event listener to task button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { newProjectModal, allProjects, currentViewedProject } from "./projects";
+import { newTaskModal } from "./todos";
 import "./styles.css";
 
 const newProject = document.querySelector("#newProject");
+const newTask = document.querySelector("#newTask");
 
 newProject.addEventListener("click", () => {
   console.log("Current Projects before new one: ", allProjects);
   console.log("Currently viewed project: ", currentViewedProject);
   newProjectModal();
+});
+
+newTask.addEventListener("click", () => {
+  newTaskModal();
 });
